8bc6680d2d7e6b72a9a0255fe4c1d9f6cb61afef
wolf-coder <cuore.fakhri@gmail.com>
2021-10-04 21:21:54 +0100

    remove SettingGit_env.sh

--- SettingGit_env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-read -p 'Type your Username: ' USR
-read -p 'Type your PAssword: ' PSSWD
-read -p 'Type your Repository link : ' Repo
-
-echo "check this line and execute it"
-echo git remote set-url origin https://$USR:$PSSWD@github.com/$USR/$Repo.git
