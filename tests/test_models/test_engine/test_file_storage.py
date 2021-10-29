0ccea1ad03d930540a8ca808dccc1b8b1e7c4cce
wolf-coder <cuore.fakhri@gmail.com>
2021-10-29 16:36:19 +0100

    testing all method

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Testing the file_storage model.
+"""
+import json
+from models.engine.file_storage import FileStorage
+import unittest
+
+class Test_FileStorage(unittest.TestCase):
+    """
+    Testing models.file_storage model.
+    """
+
+    def test_all(self):
+        """
+        Testing the `all` method:
+        must return __objects dictionary
+        """
+        storage = FileStorage()
+        obj = storage.all()
+        self.assertEqual(type(obj), dict, "the returned object must be a dictionary")
+
+
+if __name__ == '__main__':
+    unittest.main()
