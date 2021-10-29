b0ca25032c2e4df0732af4f28eeb2065f546e9c2
wolf-coder <cuore.fakhri@gmail.com>
2021-10-29 22:18:16 +0100

    Testing the `new` method:

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -5,6 +5,7 @@ Testing the file_storage model.
 import json
 from models.engine.file_storage import FileStorage
 import unittest
+from models.base_model import BaseModel
 
 
 class Test_FileStorage(unittest.TestCase):
@@ -15,12 +16,27 @@ class Test_FileStorage(unittest.TestCase):
     def test_all(self):
         """
         Testing the `all` method:
-        must return __objects dictionary
+        Must return __objects dictionary
         """
         storage = FileStorage()
         obj = storage.all()
         self.assertEqual(type(obj), dict,
                          "the returned object must be a dictionary")
+
+    def test_new(self):
+        """
+        Testing the `new` method:
+        Must sets in __objects the BaseModel obj with key <obj class name>.id
+        """
+        my_model = BaseModel()
+        storage = FileStorage()
+
+        my_model_ID = my_model.id
+        my_model_Class = my_model.__class__.__name__
+        storage.new(my_model)
+        Dict = storage.all()
+        self.assertEqual(Dict['.'.join((my_model_Class, my_model_ID))],
+                         my_model)
 
 
 if __name__ == '__main__':
