912cbe2c5c9dc46848cd0b623a02d56cf2460e0f
wolf-coder <cuore.fakhri@gmail.com>
2021-10-30 21:57:55 +0100

    testing test_reload(self):

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -43,6 +43,7 @@ class Test_FileStorage(unittest.TestCase):
         """
         This method must serializes __objects to the JSON file
         (path: __file_path)
+        TODO:THE FOLLOWING TEST IS NOT VALID
         """
         my_model = BaseModel()
         storage = FileStorage()
@@ -54,6 +55,26 @@ class Test_FileStorage(unittest.TestCase):
         Dict = storage.all()
         self.assertEqual(Dict['.'.join((my_model_Class, my_model_ID))],
                          my_model)
+
+    def test_reload(self):
+        """
+        Deserializes the JSON file to __objects
+        (only if the JSON file (__file_path) exists;
+        otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)
+        """
+        my_model = BaseModel()
+        storage = FileStorage()
+
+        my_model_ID = my_model.id
+        my_model_Class = my_model.__class__.__name__
+        storage.new(my_model)
+        storage.save()
+        Dict_before_reload = storage.all()
+        storage.reload()
+        Dict_after_reload = storage.all()
+        self.assertCountEqual(Dict_after_reload.keys(),
+                              Dict_before_reload.keys())
 
 
 if __name__ == '__main__':
