676d6a3ed4d4b0df1bc009d29a8f892fc51faab9
wolf-coder <cuore.fakhri@gmail.com>
2021-11-09 19:00:40 +0100

    Testing User inherit from BaseModel

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Testing the user (user.py) model.
+"""
+import unittest
+from models.user import User, BaseModel
+
+
+class Test_User(unittest.TestCase):
+    """
+    Testing models.User model.
+    """
+    def test_inheritance(self):
+        """
+        Testing User inherit from BaseModel
+        """
+        self.assertTrue(issubclass(User, BaseModel))
+        my_user = User()
+        self.assertTrue('last_name' in User.__dict__.keys())
+
+
+if __name__ == '__main__':
+    unittest.main()
