cee215d962e65835721ab316a8ba5161e51c058e
wolf-coder <cuore.fakhri@gmail.com>
2021-11-09 19:04:33 +0100

    check

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -16,8 +16,6 @@ class Test_User(unittest.TestCase):
         """
         self.assertTrue(issubclass(User, BaseModel))
         my_user = User()
-        my_user.last_name = "ali"
-        self.assertTrue('last_name' in User.__dict__.keys())
 
 
 if __name__ == '__main__':
