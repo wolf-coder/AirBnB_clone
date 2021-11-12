ac9b9fab952858351519ff82bd866cd1dad2027b
wolf-coder <cuore.fakhri@gmail.com>
2021-11-12 11:37:54 +0100

    fix

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Testing the user (user.py) model.
+Testing the user (review.py) model.
 """
 import unittest
 from models.review import Review, BaseModel
@@ -15,9 +15,9 @@ class Test_modules(unittest.TestCase):
         Testing State: 'name' class attribute
         """
         self.assertTrue(issubclass(Review, BaseModel))
-        self.assertTrue(type("place_id") == str)
-        self.assertTrue(type("user_id") == str)
-        self.assertTrue(type("text") == str)
+        self.assertTrue(type(Review.place_id) == str)
+        self.assertTrue(type(Review.user_id) == str)
+        self.assertTrue(type(Review.text) == str)
 
 
 if __name__ == '__main__':
